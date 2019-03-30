10213936dee71d178c3a8e4a1d01f06a03296e3c
chrismoral3 <48483029+chrismoral3@users.noreply.github.com>
2019-03-30 13:56:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var global_urls = {
+  constant: "https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=",
   current:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=my+current+location",
   stores:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=stores+near+me",
   daycare:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=childcare+near+me",
-  publictransp:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=public+transportation+near+me",}
+  publictransp:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=25+park+place",}
 
 function change_src(n) {
   switch(n) {
@@ -19,4 +20,15 @@ function change_src(n) {
     document.getElementById("map_iframe").src=global_urls.publictransp;
     break;
   }
+}
+
+var address_input = null
+
+function change_src_address() {
+    if(address_input == null) {
+      address_input = document.getElementById("adrsfield");
+    }
+    var output = global_urls.constant+address_input.value.replace(/ /g,"+");
+    console.log(output)
+    map_iframe.src=output;
 }
