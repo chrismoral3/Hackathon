97f21977facebdf13cd591ba24c15f666a822ad2
Daniel Grinshpon <GrinshponDaniel@protonmail.com>
2019-03-30 13:06:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 var global_urls = {
-  constant:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=",
+  constant: "https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=",
   current:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=my+current+location",
   stores:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=stores+near+me",
-  daycare:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=9712+north+pond"}
-
-var map_iframe = document.getElementById("map_iframe");
+  daycare:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=childcare+near+me",
+  publictransp:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=25+park+place",}
 
 function change_src(n) {
   switch(n) {
     case 0:
-    map_iframe.src=global_urls.current;
+    document.getElementById("map_iframe").src=global_urls.current;
     break;
     case 1:
-    map_iframe.src=global_urls.stores;
+    document.getElementById("map_iframe").src=global_urls.stores;
     break;
     case 2:
-    map_iframe.src=global_urls.daycare;
+    document.getElementById("map_iframe").src=global_urls.daycare;
+    break;
+    case 3:
+    document.getElementById("map_iframe").src=global_urls.publictransp;
     break;
   }
 }
 
-var address_input = document.getElementBtId("address_field");
+var address_input = null
 
 function change_src_address() {
-    map_iframe.src=global_urls.constant+address_input.value.replace(/ /g,"+");
-}
-
-/*address_input.addEventListener("keydown",function(event) {
-    if(event.keycode === 13) {// 13 - space
-	//event.preventDefault();
-	change_src_address(address_input.value.replace(/ /g,"+");
+    if(address_input == null) {
+      address_input = document.getElementById("adrsfield");
     }
-});
-*/
-    
+    var output = global_urls.constant+address_input.value.replace(/ /g,"+");
+    console.log(output)
+    map_iframe.src=output;
+}
