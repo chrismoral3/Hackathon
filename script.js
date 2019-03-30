7b4fc55873b14c35f16ea9ab9a1bbbd6530bd2c1
Daniel Grinshpon <GrinshponDaniel@protonmail.com>
2019-03-30 12:38:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 var global_urls = {
+  constant:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=",
   current:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=my+current+location",
   stores:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=stores+near+me",
-  daycare:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=childcare+near+me",
-  publictransp:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=public+transportation+near+me",}
+  daycare:"https://www.google.com/maps/embed/v1/search?key=AIzaSyDHUvAP5UqYKG_YEravAYaI1vccBDj8CvY&q=9712+north+pond"}
+
+var map_iframe = document.getElementById("map_iframe");
 
 function change_src(n) {
   switch(n) {
     case 0:
-    document.getElementById("map_iframe").src=global_urls.current;
+    map_iframe.src=global_urls.current;
     break;
     case 1:
-    document.getElementById("map_iframe").src=global_urls.stores;
+    map_iframe.src=global_urls.stores;
     break;
     case 2:
-    document.getElementById("map_iframe").src=global_urls.daycare;
-    break;
-    case 3:
-    document.getElementById("map_iframe").src=global_urls.publictransp;
+    map_iframe.src=global_urls.daycare;
     break;
   }
 }
+
+var address_input = document.getElementBtId("address_field");
+
+function change_src_address() {
+    map_iframe.src=global_urls.constant+address_input.value.replace(/ /g,"+");
+}
+
+/*address_input.addEventListener("keydown",function(event) {
+    if(event.keycode === 13) {// 13 - space
+	//event.preventDefault();
+	change_src_address(address_input.value.replace(/ /g,"+");
+    }
+});
+*/
+    
